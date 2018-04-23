71bc29b17d8175628011694ff69e609376df9221
Homum Ahsan <hahsan95@gmail.com>
2018-04-23 11:51:28 -0500

    passing it over

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,50 @@
+// function bubbleSort(array) {
+//   let numOfSwaps = 0;
+//   for (let i = 0; i < array.length; i++){
+//     if (i !== array.length) {
+//       if (array[i] > array[i+1]) {
+//         let store = array[i];
+//         array[i] = array[i+1];
+//         array[i+1] = store;
+//         numOfSwaps++;
+//       }
+//     }
+//   }
+
+//   if (numOfSwaps !== 0){
+//     bubbleSort(array);
+//   }
+
+//   return array;
+
+// }
+
+let max = [];
+
 function bubbleSort(array) {
-//test;
+  let maxNum = 0;
+  for (let i = 0; i < array.length; i++){
+    if (maxNum < array[i]){
+      maxNum = array[i];
+
+    }
+    if (array[i] > array[i+1]) {
+      let store = array[i];
+      array[i] = array[i+1];
+      array[i+1] = store;
+    }
+  }
+
+  if (array[array.length] === maxNum){
+    max.push(array.pop());
+  }
+
+  if (array.length === 1) {
+    array.concat(max);
+    return array;
+  }
+  else {
+    bubbleSort(array);
+  }
 }
+
