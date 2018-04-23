b9927cb220785f1bc0cd5c711c1468471b7a34ea
Homum Ahsan <hahsan95@gmail.com>
2018-04-23 13:36:27 -0500

    test

--- bubblesort.js
+++ bubblesort.js
@@ -1,54 +1,51 @@
-function bubbleSort (array) {
-  let numOfSwaps = 0
-  for (let i = 0; i < array.length; i++) {
-    if (i !== array.length) {
-      if (array[i] > array[i + 1]) {
-        let store = array[i]
-        array[i] = array[i + 1]
-        array[i + 1] = store
-        numOfSwaps++
-      }
-    }
-  }
-
-  if (numOfSwaps !== 0) {
-    bubbleSort(array)
-  }
-
-  return array
-}
-
-let max = []
-
-function bubbleSort (array) {
-  let maxNum = 0
-  let numOfSwaps = 0
-  let length = array.length;
-
-  if (!array.length) {
-    return array
-  }
-  for (let i = 0; i < length; i++) {
-    if (maxNum < array[i]) {
-      maxNum = array[i]
-      console.log(maxNum)
-    }
-  }
-  if (array[length] === maxNum){
-    length--;
-  }
-}
-
-//     if (array[i] > array[i+1]) {
-//       let store = array[i]
-//       array[i] = array[i+1]
-//       array[i+1] = store
+// function bubbleSort (array) {
+//   let numOfSwaps = 0
+//   for (let i = 0; i < array.length; i++) {
+//     if (i !== array.length) {
+//       if (array[i] > array[i + 1]) {
+//         let store = array[i]
+//         array[i] = array[i + 1]
+//         array[i + 1] = store
+//         numOfSwaps++
+//       }
 //     }
 //   }
 
-
-
-//   if (array.length === 1) {
-//     array.concat(max)
-//     return array
+//   if (numOfSwaps !== 0) {
+//     bubbleSort(array)
 //   }
+
+//   return array
+// }
+
+/*
+alternate solution
+*/
+
+function bubbleSort (array) {
+  let length = array.length+4;
+  console.log(length);
+  while (length > 0){
+    length--
+    for (let i = 0; i < length; i++) {
+      let maxNum = 0;
+      if (maxNum < array[i]) {
+        maxNum = array[i];
+      }
+
+      if (i !== array.length) {
+        if (array[i] > array[i + 1]) {
+          let store = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = store;
+        }
+      }
+      if (array[length] === maxNum){
+            length--;
+      }
+    }
+  }
+  return array;
+}
+
+bubbleSort([1,3,2,5,4]);
